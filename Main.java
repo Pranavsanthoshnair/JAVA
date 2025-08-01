3ea766a64c65bebd3f9dd8fcfcaf27c07255f4e4
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-08-01 10:02:56 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+/*
+ * Name - Pranav S Nair
+ * Date - 1/8/25
+ * Purpose - To print student details and print total no of students.
+ */
+class Student{
+	String name;
+	int rollNumber;
+	static int studentCount;
+	Student(String name,int rollNumber){
+		this.name=name;
+		this.rollNumber=rollNumber;
+		studentCount++;
+	}
+	public static void displayStudentCount() {
+		System.out.println("Total No of Students: "+studentCount+"\n");
+	}
+	public void displayInfo() {
+		System.out.println("Name : "+name);
+		System.out.println("Roll No : "+rollNumber);
+	}
+}
+public class Main {
+	public static void main(String[] args) {
+	Student s1 = new Student("John Doe",16);
+	s1.displayInfo();
+	Student.displayStudentCount();
+	
+	Student s2 = new Student("Jose Tom",23);
+	s2.displayInfo();
+	Student.displayStudentCount();
+	
+	Student s3 = new Student("Rifan S",62);
+	s3.displayInfo();
+	Student.displayStudentCount();
+	}
+}
