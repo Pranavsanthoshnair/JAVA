a1d297b33cd03c68c9e43e879b795c9ca9e4bc96
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-08-01 10:51:38 +0530

    Add files via upload

---
+++ BookStore.java
@@ -0,0 +1,52 @@
+class Book{
+	String title;
+	String author;
+	final int bookID;
+	static int bookNo;
+	static int bookCounter=100;
+	boolean showLibrary;
+	static final String  library_name="Central Library";
+	Book(){
+		this.title=title;
+		this.author=author;
+		this.bookID=bookCounter;
+	}
+	Book(String title,String author){aswe3d
+		this.title=title;
+		this.author=author;
+		bookID=bookCounter++;
+		bookNo++;
+	}
+	public void displayInfo() {
+		System.out.println("Title: "+title);
+		System.out.println("Author: "+author);
+		System.out.println("Book ID: "+bookID);
+	}
+	public void displayInfo(boolean showLibrary) {
+		if (showLibrary == true) {
+			System.out.println("Library Name: "+ "Central Library");
+		}
+		else {
+			System.out.println("Library Name: "+ "None");
+		}
+	}
+	public static void displayTotalBooks() {
+		System.out.println("Total No of Books: "+ bookNo);
+	}
+}
+public class BookStore {
+	public static void main(String[] args) {
+	Book b1 = new Book("No body Cares", "Somebody");
+	b1.displayInfo();
+	b1.displayInfo(true);
+	b1.displayTotalBooks();
+	Book b2 = new Book("No body Cares", "Somebody");
+	b2.displayInfo();
+	b2.displayInfo(true);
+	b2.displayTotalBooks();
+	Book b3 = new Book("No body Cares", "Somebody");
+	b3.displayInfo();
+	b3.displayInfo(true);
+	b3.displayTotalBooks();
+	}
+}
